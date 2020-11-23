ee5712f64acc7d2a9c6ac19cf59fa1707be5afb6
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2020-11-23 13:49:20 +0100

    TestDrivenDevelopment OpretProfil
    I denne udgive af vores produkt, er der blevet tilføjet en unittest til Opret Profil userstory. Der er testet GetAll, GetId og Post, hvilket er de tre funktion som er en del af denne userstory.

---
+++ KUBike_REST/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KUBike_REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<CyclesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CyclesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CyclesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CyclesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CyclesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ KUBike_RESTTests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KUBike_REST.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KUBike_REST.Controllers.Tests
+{
+    [TestClass()]
+    public class UsersControllerTests
+    {
+        UsersController cmd = new UsersController();
+        [TestMethod()]
+        //For at testet GETALL funktionen i unittest. Vi tester dette igennem når vi tjekker for mange items der er i listen. Derfor bruger vi Assert.AreEqual, til at se om de har samme antal af items i listen. 
+        public void GetTest()
+        {
+            Assert.AreEqual(3, cmd.Get.Count());
+        }
+        //Her tester vi GetId funktionen. Vi tester dette igennem en Assert.AreEqual metode og Assert.IsNotNull. Igennem disse to requests finder vi ud af om itemet exist og om den har id'et 2.
+        [TestMethod()]
+        public void GetIdTest()
+        {
+            Assert.AreEqual(2, cmd.Get(2).Id);
+            Assert.IsNotNull(cmd.Get(2));
+        }
+
+        //Her tester vi Post funktionen. Vi tester dette igennem en Assert.AreEqual metode. Men før vi benytter os af denne metode opretter vi et objekt som skal bruges i testen. I er objektet vi benytte til at tjekke og telefonnummet stå over ens med telefonnummet vi har angivet i vores objekt.              
+        [TestMethod()]
+        public void PostTest()
+        {
+            User i = new User(1, "Dummy", "dummy", "dummy", "dummy", 70707070, 1);
+            cmd.Post(i);
+            Assert.AreEqual(70707070, cmd.Get(1).Mobile);
+
+        }
+
+    }
+}
